32e2a1ef194c8a70b1bd4465d76f9ec595665726
ALEXSANDER2002 <alex13filipi@gmail.com>
2025-04-07 11:45:40 -0300

    aaaaaaaaaaa

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,17 +1,16 @@
 import { createClient } from "@supabase/supabase-js"
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error("NEXT_PUBLIC_SUPABASE_URL is required")
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
 }
-
 if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error("NEXT_PUBLIC_SUPABASE_ANON_KEY is required")
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
 }
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 // Tipos para as tabelas do Supabase
 export type Contact = {
